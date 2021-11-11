f438d62d1941a3d3d27c2cdd58ca9449989dcc3a
slawekradzyminski <slawomir.radzyminski@gmail.com>
2021-11-11 22:20:58 +0100

    added pop test

--- src/test/java/pl/testuj/selenium/tests/ArenaTest.java
+++ src/test/java/pl/testuj/selenium/tests/ArenaTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import pl.testuj.selenium.pages.HomePage;
 
 public class ArenaTest {
 
@@ -32,6 +33,12 @@ public class ArenaTest {
     @Test
     public void myFirstSeleniumTest() {
         Assertions.assertThat(driver.getTitle()).contains("TestArena");
+    }
+
+    @Test
+    public void myFirstPOPSeleniumTest() {
+        HomePage homePage = new HomePage(driver);
+        homePage.login();
     }
 
 }
