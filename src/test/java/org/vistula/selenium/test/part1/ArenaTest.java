41a690c1205e811c61fdb3ef8ab338d379556f64
slawekradzyminski <slawomir.radzyminski@gmail.com>
2021-11-11 21:58:06 +0100

    Bumped java

--- src/test/java/org/vistula/selenium/test/part1/ArenaTest.java
+++ src/test/java/org/vistula/selenium/test/part1/ArenaTest.java
@@ -2,49 +2,36 @@ package org.vistula.selenium.test.part1;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.assertj.core.api.Assertions;
-import org.junit.BeforeClass;
-import org.junit.Test;
-import org.openqa.selenium.By;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class ArenaTest {
 
     private WebDriver driver;
 
-    @BeforeClass
+    @BeforeAll
     public static void setDriver() {
         WebDriverManager.chromedriver().setup();
     }
 
-    @Test
-    public void myFirstSeleniumTest() {
+    @BeforeEach
+    public void openPage() {
         driver = new ChromeDriver();
         driver.get("http://demo.testarena.pl/zaloguj");
-        Assertions.assertThat(driver.getTitle()).contains("TestArena");
+    }
+
+    @AfterEach
+    public void closeDriver() {
         driver.quit();
     }
 
     @Test
-    public void myFirstInteractionTest() {
-        driver = new ChromeDriver();
-        driver.get("http://demo.testarena.pl/zaloguj");
-        WebElement email = driver.findElement(By.id("email"));
-        WebElement password = driver.findElement(By.id("password"));
-        WebElement login = driver.findElement(By.id("login"));
-
-        email.sendKeys("administrator@testarena.pl");
-        password.sendKeys("sumXQQ72$L");
-        login.click();
-
-        new WebDriverWait(driver, 3)
-                .until(ExpectedConditions.presenceOfElementLocated(By.id("footer")));
-
-        Assertions.assertThat(driver.getTitle()).contains("Kokpit");
-        driver.quit();
+    public void myFirstSeleniumTest() {
+        Assertions.assertThat(driver.getTitle()).contains("TestArena");
     }
 
 }
