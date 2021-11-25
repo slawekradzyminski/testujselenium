284ca31db7515a16264a11c2aee2ae2d6ef00a94
slawekradzyminski <slawomir.radzyminski@gmail.com>
2021-11-25 13:44:11 +0100

    Day2-4 - working with multiple windows

--- src/test/java/pl/testuj/selenium/tests/AwesomeTestingTest.java
+++ src/test/java/pl/testuj/selenium/tests/AwesomeTestingTest.java
@@ -6,9 +6,11 @@ import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Cookie;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.WindowType;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 
 import java.util.List;
+import java.util.Set;
 
 public class AwesomeTestingTest extends AbstractTest {
 
@@ -42,6 +44,29 @@ public class AwesomeTestingTest extends AbstractTest {
         driver.findElement(By.linkText("Cypress")).click();
         waitForResults();
         verifyNumberOfPosts(1);
+    }
+
+    @Test
+    public void shouldDisplayAddCommentSection() {
+        driver.findElements(By.className("post-title")).get(0).click();
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.className("blogger-comment-from-post")));
+    }
+
+    // https://stackoverflow.com/questions/9588827/how-to-switch-to-the-new-browser-window-which-opens-after-click-on-the-button
+    @Test
+    public void shouldCorrectlyOpenGitHub() {
+        driver.findElements(By.cssSelector(".HTML a img")).get(3).click();
+        wait.until(ExpectedConditions.numberOfWindowsToBe(2));
+
+        String mainWindowHandle = driver.getWindowHandle();
+        String newlyOpenedWindowHandle = driver.getWindowHandles().stream()
+                .filter(id -> !id.equals(mainWindowHandle))
+                .findFirst().get();
+
+        driver.switchTo().window(newlyOpenedWindowHandle);
+        Assertions.assertThat(driver.getTitle()).contains("GitHub");
+        driver.switchTo().window(mainWindowHandle);
+        Assertions.assertThat(driver.getTitle()).contains("Awesome Testing");
     }
 
     private void waitForResults() {
