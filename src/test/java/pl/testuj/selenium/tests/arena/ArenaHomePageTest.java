91460301506b91357e2e55d2eb6f3d9e5e315661
slawekradzyminski <slawomir.radzyminski@gmail.com>
2021-11-25 15:30:02 +0100

    Day2-7 test arena project add

--- src/test/java/pl/testuj/selenium/tests/arena/ArenaHomePageTest.java
+++ src/test/java/pl/testuj/selenium/tests/arena/ArenaHomePageTest.java
@@ -1,10 +1,14 @@
 package pl.testuj.selenium.tests.arena;
 
 import org.apache.commons.lang.RandomStringUtils;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import pl.testuj.selenium.tests.AbstractTest;
+
+import java.util.List;
 
 public class ArenaHomePageTest extends AbstractTest {
 
@@ -26,10 +30,23 @@ public class ArenaHomePageTest extends AbstractTest {
         driver.findElement(By.cssSelector("[title=Administracja]")).click();
         driver.findElements(By.className("button_link")).get(0).click();
 
-        driver.findElement(By.id("name")).sendKeys(RandomStringUtils.randomAlphabetic(10));
+        String projectName = RandomStringUtils.randomAlphabetic(10);
+
+        driver.findElement(By.id("name")).sendKeys(projectName);
         driver.findElement(By.id("prefix")).sendKeys(RandomStringUtils.randomAlphabetic(6));
         driver.findElement(By.id("description")).sendKeys(RandomStringUtils.randomAlphabetic(20));
         driver.findElement(By.id("save")).click();
+
+        driver.findElement(By.cssSelector("[title=Kokpit]")).click();
+        driver.findElement(By.cssSelector("[title=Administracja]")).click();
+        driver.findElement(By.id("search")).sendKeys(projectName);
+        driver.findElement(By.id("j_searchButton")).click();
+
+        List<WebElement> projectsFound = driver.findElements(By.cssSelector("tbody tr"));
+        Assertions.assertThat(projectsFound).hasSize(1);
+
+        WebElement firstElementTitle = driver.findElements(By.cssSelector("tbody tr td")).get(0);
+        Assertions.assertThat(firstElementTitle.getText()).isEqualTo(projectName);
     }
 
 }
